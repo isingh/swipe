0b396037743970bba4db905d358ca11e59056c74
Inderpreet Singh <inderpreet.singh@tapjoy.com>
2012-10-18 11:31:44 -0700

    Initial empty Rails repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swipe::Application.config.secret_token = 'eb6e533075ad61edcb59b0ba868cdffdbaf20fefc08169d828d1350381950112d262b07d3d8be5d1e8a461f54429936c2a46954f9c14ed6db2e1f494615112fe'
