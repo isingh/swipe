bd74be5a1a641e1d472dc17038d0b7e04b1954b4
Inderpreet Singh <inderpreet.singh@tapjoy.com>
2012-10-19 16:19:01 -0700

    Add offers model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121019210503) do
+ActiveRecord::Schema.define(:version => 20121019231011) do
+
+  create_table "advertiser_offers", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "url"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "transactions", :force => true do |t|
     t.integer  "user_card_id"
